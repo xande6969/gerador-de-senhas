f2ceafdc853db93e57cba04b59279fdc6aaba277
xande6969 <alexandre.medeiros.domingues@escola.pr.gov.br>
2026-08-27 12:20:24 -0300

    botoes aumenta e diminui

--- main.js
+++ main.js
@@ -1,2 +1,21 @@
 const numeroSenha = document.querySelector('.parametro-senha__texto')
-numeroSenha.textcontent = 5;
+
+
+tamanhoSenha = 8
+numeroSenha.textContent = tamanhoSenha;
+
+const botoes = document.querySelectorAll('.parametro-senha__botao')
+
+botoes[0].onclick = diminuir;
+
+function diminuir(){
+    tamanhoSenha--;
+    numeroSenha.textContent = tamanhoSenha;
+}
+
+botoes[1].onclick = aumentar;
+
+function aumentar(){
+    tamanhoSenha++;
+    numeroSenha.textContent = tamanhoSenha;
+}
