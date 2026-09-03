8aac969090446c264d6cf70a144a51ea5890c407
xande6969 <alexandre.medeiros.domingues@escola.pr.gov.br>
2026-09-03 12:38:37 -0300

    gerador de senha

--- main.js
+++ main.js
@@ -4,18 +4,25 @@ const campoSenha = document.querySelector('#campo-senha')
 campoSenha.value = 'aqui vai ser a senha'
 
 let letrasMaiusculas = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ'
-/ / minusculas, numeros e simbolos
+
+let letrasMinusculas = 'abcdefghilklmnopqrstuvwxyz'
+
+let numero "123456789"
+
+let simbolos "!@#$%&*"
 
 tamanhoSenha = 8
 numeroSenha.textContent = tamanhoSenha;
 
 const botoes = document.querySelectorAll('.parametro-senha__botao')
 
+
 botoes[0].onclick = diminuir;
 
 function diminuir(){
     tamanhoSenha--;
     numeroSenha.textContent = tamanhoSenha;
+    geraSenha()
 }
 
 botoes[1].onclick = aumentar;
@@ -23,4 +30,16 @@ botoes[1].onclick = aumentar;
 function aumentar(){
     tamanhoSenha++;
     numeroSenha.textContent = tamanhoSenha;
+}
+geraSenha()
+
+function geraSenha(){
+    let sennha = ""
+
+    for(let i = 0; i < tamanhoSenha; i++){
+        let numeroAleatorio = Math.random() * 26;
+        numeroAleatorio = Math.floor(numeroAleatorio)
+            senha = senha + letrasMaiusculas[numeroAleatorio]
+    }
+    campoSenha.value = senha;
 }
