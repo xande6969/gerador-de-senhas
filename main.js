6ab5eed6a135a47433f92fae608d5879ac835ee8
xande6969 <alexandre.medeiros.domingues@escola.pr.gov.br>
2026-09-25 11:03:15 -0300

    vampeta

--- main.js
+++ main.js
@@ -62,12 +62,33 @@ function aumentar(){
 
 
 function geraSenha(){
-    let senha = ""
+    
+    let alfabeto = ''
+
+    
+    if(checkbox[0].checked){
+       alfabeto = alfabeto + letrasMaiusculas
+    }
+    
+     if(checkbox[1].checked){
+       alfabeto = alfabeto + letrasMinusculas
+    }
+    
+     if(checkbox[2].checked){
+       alfabeto = alfabeto + numero
+    }
+    
+     if(checkbox[3].checked){
+       alfabeto = alfabeto + simbolos
+    }
+
+
+    let senha = ''
 
     for(let i = 0; i < tamanhoSenha; i++){
-        let numeroAleatorio = Math.random() * 26;
+        let numeroAleatorio = Math.random() * alfabeto.length;
         numeroAleatorio = Math.floor(numeroAleatorio)
-            senha = senha + letrasMaiusculas[numeroAleatorio]
+            senha = senha + alfabeto[numeroAleatorio]
     }
     campoSenha.value = senha;
     classificarSenha()
